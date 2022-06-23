4993a4539361e7dabcb247c899521aec7cf1fbbe
jhorbee01 <105235984+jhorbee01@users.noreply.github.com>
2022-06-23 19:29:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const details = {
+  firstName: "Abiodun",
+  lastName: "Jobi",
+  height: "1.77meters",
+  country: "Nigeria",
+};
+alert(
+  "Name: " +
+    details.firstName +
+    " " +
+    details.lastName +
+    " " +
+    "Height: " +
+    details.height +
+    " " +
+    "Country: " +
+    details.country
+);
